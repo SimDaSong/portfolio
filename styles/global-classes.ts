5dd16c5cbcbb16313670c032cbc03e9651556166
SimDaSong <song98987@korea.ac.kr>
2021-11-18 18:31:33 +0900

    feat: global classes 추가

---
+++ styles/global-classes.ts
@@ -0,0 +1,12 @@
+/**
+ * @todo: 글로벌 클래스를 어떻게 관리할지 고민
+ * @body: 값들만 쓸건데 클래스로 선언해서 지역별 클래스에 상속하는 게 맞는건지 이렇게 쓰는 게 맞는건지 ..
+*/
+const globalClasses = {
+    tableWrap: "px-4 py-5 border-b border-gray-200 sm:grid sm:grid-cols-5 sm:gap-4 sm:px-6",
+    text: "text-xl font-bold text-gray-900 sm:mt-0 lg:text-m",
+    subText: "text-sm font-large text-gray-500",
+    link: "underline"
+}
+
+export default globalClasses;
