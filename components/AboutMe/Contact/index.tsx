786b81c97253183ed64baeee2339c31d881fbdc8
SimDaSong <song98987@korea.ac.kr>
2021-11-28 23:22:14 +0900

    fix: contact 파일 구조 변경

--- components/AboutMe/Contact/index.tsx
+++ components/AboutMe/Contact/index.tsx
@@ -2,25 +2,27 @@ import React from "react";
 
 import globalClasses from "@/styles/global-classes";
 import SubTitle from "@/components/SubTitle";
-import { contacts } from "@/utils/data";
+import { contacts } from "./data";
 
 type Props = {
   className: string;
 };
 
 const classes = {
-  contactWrap: "grid grid-rows-1 gap-6 lg:px-6",
+  contactWrap: "grid grid-rows-1 gap-5 lg:px-6",
+  contactContentWrap: "leading-7",
+  contactName: "font-bold text-default mr-2",
 };
 
 const Contact = (props: Props) => {
   return (
     <div className={`${props.className} ${classes.contactWrap}`}>
       <SubTitle>CONTACT</SubTitle>
-      <div>
+      <div className={classes.contactContentWrap}>
         {contacts.map((contact, idx) => (
           <p key={idx} className="break-all">
-            <span className="font-bold">{contact.name}: </span>
-            <span className={globalClasses.link}>
+            <span className={classes.contactName}>{contact.name}</span>
+            <span className={`${globalClasses.link}`}>
               <a href={contact.link}>{contact.text || contact.link}</a>
             </span>
           </p>
