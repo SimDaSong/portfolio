e06da00a7d3886a4883909224670fc1de14d3c24
SimDaSong <song98987@korea.ac.kr>
2021-11-29 11:33:54 +0900

    fix: Layout 컴포넌트 수정
    Head는 _app으로 빼고 class 추가

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,19 +1,13 @@
 import React, { ReactNode } from "react";
-import Head from "next/head";
 
 type Props = {
   children?: ReactNode;
 };
 
 const Layout = ({ children }: Props) => (
-  <div>
-    <Head>
-      <title>심다송의 포트폴리오</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    {children}
-  </div>
+    <div className="px-10 mt-8 mb-16 max-w-7xl py-8 lg:px-16">
+        {children}
+    </div>
 );
 
 Layout.defaultProps = {
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
 import Layout from "@/components/Layout";
+import Head from "next/head";
+import React from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>심다송의 포트폴리오</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
