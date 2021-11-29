47e99ca6b4c7a33bffe417f7160d5791a0d5234f
SimDaSong <song98987@korea.ac.kr>
2021-11-29 12:42:23 +0900

    chore: webpack path alias 적용

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 module.exports = {
   reactStrictMode: true,
+  webpack(config, options) {
+    config.resolve.alias['@'] = path.join(__dirname, '.')
+    return config
+  }
 }
