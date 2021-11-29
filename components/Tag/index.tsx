6984c8f4e0b0a8ed11c9e4b2d6f46ed3ea057ca2
SimDaSong <song98987@korea.ac.kr>
2021-11-29 10:53:09 +0900

    feat: Tag 컴포넌트 구현

---
+++ components/Tag/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type Props = {
+  children: string;
+};
+
+const Tag = (props: Props) => {
+  const { children } = props;
+
+  return (
+    <span className="bg-default text-white px-2 py-0.5 rounded-xl text-xs mr-1 inline-block mt-1">
+      {children}
+    </span>
+  );
+};
+
+export default Tag;
