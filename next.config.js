96a8e96f4a8a4112d2d742e583882023ac4dc230
SimDaSong <song98987@korea.ac.kr>
2021-11-30 17:54:45 +0900

    chore: mdx 모듈 설치 및 설정

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const path = require('path');
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx$/,
+})
 
-module.exports = {
-  reactStrictMode: true,
-  webpack(config, options) {
-    config.resolve.alias['@'] = path.join(__dirname, '.')
-    return config
+module.exports = withMDX(
+  {
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+    reactStrictMode: true,
+    webpack: config => {
+      config.resolve.alias['@'] = path.join(__dirname, '.')
+      config.module.rules.push({test: /\.md$/, use: "raw-loader"});
+      return config;
+    }
   }
-}
+)
