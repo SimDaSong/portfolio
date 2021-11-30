40312202bb835a1fc662a38a6c65edd5422b1614
SimDaSong <song98987@korea.ac.kr>
2021-11-30 19:45:47 +0900

    chore: fix typo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
-import type { NextPage } from "next";
-
+import { NextPage } from "next";
 import React from "react";
 import AboutMe from "@/components/AboutMe";
 import Career from "@/components/Career";
